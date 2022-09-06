32464f72905418f7cc8fa3d00d7df0d22caff82b
sztompka0206 <ma3san27@outlook.jp>
2022-09-06 17:18:33 +0900

    test_commit

---
+++ test1/target/debug/deps/test1-3b16ad1d46a6fd99.d
@@ -0,0 +1,5 @@
+/home/nakata/rust/test1/target/debug/deps/test1-3b16ad1d46a6fd99: src/main.rs
+
+/home/nakata/rust/test1/target/debug/deps/test1-3b16ad1d46a6fd99.d: src/main.rs
+
+src/main.rs:
---
+++ test1/target/debug/test1.d
@@ -0,0 +1,1 @@
+/home/nakata/rust/test1/target/debug/test1: /home/nakata/rust/test1/src/main.rs
